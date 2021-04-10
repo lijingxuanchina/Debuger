34b6213e30d5e9d4b974389455492d6c480107ed
lijingxuanchina <847720711@qq.com>
2021-04-11 00:45:28 +0800

    【项目：Debuger】【bugID：0#】【原因：分级管理cmake文件，改善cmake文件结构】【影响：能否生成PCH】

--- cmake/debuger.cmake
+++ cmake/debuger.cmake
@@ -1,1 +1,10 @@
-message("-- Run cmake scripts")
+SET(CMAKE_SCRIPTS_PATH "${CMAKE_CURRENT_BINARY_DIR}/../Debuger/cmake")
+
+INCLUDE(${CMAKE_SCRIPTS_PATH}/addpch.cmake)
+
+MACRO(RUN_CMAKE_SCRIPTS)
+	message("-- Run cmake scripts")
+
+	# This macro from cmake/addpch.cmake
+	ADD_PCH()
+ENDMACRO(RUN_CMAKE_SCRIPTS)
